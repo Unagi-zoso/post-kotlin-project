73dae76bc732577b3f16337af91b5f0845f5378d
Unagi_zoso <Unagi.zoso@gmail.com>
2023-11-05 22:30:54 +0900

    Feat: PostUpdateRequest dto 클래스 추가

---
+++ src/main/kotlin/com/kotlin/project/postsnsproject/controller/dto/PostUpdateRequest.kt
@@ -0,0 +1,11 @@
+package com.kotlin.project.postsnsproject.controller.dto
+
+/**
+ * @author : Unagi_zoso
+ * @date : 2023-11-05
+ */
+data class PostUpdateRequest(
+    val title: String,
+    val content: String,
+    val updatedBy: String,
+)
