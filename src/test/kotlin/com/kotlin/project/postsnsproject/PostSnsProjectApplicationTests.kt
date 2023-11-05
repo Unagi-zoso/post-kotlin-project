c999640d0c55035eeef13df6955a9bd693822c75
Unagi_zoso <Unagi.zoso@gmail.com>
2023-11-05 17:39:36 +0900

    Feat: first commit

---
+++ src/test/kotlin/com/kotlin/project/postsnsproject/PostSnsProjectApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.kotlin.project.postsnsproject
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PostSnsProjectApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
---
+++ src/test/kotlin/com/kotlin/project/postsnsproject/SampleTest.kt
@@ -0,0 +1,14 @@
+package com.kotlin.project.postsnsproject
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+
+/**
+ * @author : Unagi_zoso
+ * @date : 2023-11-05
+ */
+class SampleTest : FunSpec({
+    test("sample test") {
+        1 shouldBe 1
+    }
+})
